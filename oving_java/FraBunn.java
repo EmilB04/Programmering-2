a30b23f0ae35bd1204b17a411684d0be11d550a4
EmilB04 <emil.berglund@live.no>
2023-12-23 12:14:29 +0100

    Øving

---
+++ oving_java/FraBunn.java
@@ -0,0 +1,5 @@
+public class FraBunn {
+    public static void main(String[] args) {
+        System.out.println("Dette er mitt første Java program som jeg har skrevet fra bunnen av");
+    }
+}
---
+++ oving_java/HelloYou.java
@@ -0,0 +1,17 @@
+public class HelloYou {
+    // Klassenavnet (ln1) må matche filnavnet
+    public static void main(String[] args) {
+        System.out.println("Hello Emil");
+        System.out.println("Linje 2");
+        System.out.println("Linje 3");
+        System.out.println("Linje 4");
+        // Skriver ut på ny linje 
+
+        System.out.print("God ");
+        System.out.print("Jul!");
+        // Skriver ut på samme linje 
+    }
+}
+
+// Compile = javac HelloYou.java
+// Run compiled java = java HelloYou
