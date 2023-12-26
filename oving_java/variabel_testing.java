753b4cf8ed90322e64661daeaf1d5356ee2e23cd
EmilB04 <emil.berglund@live.no>
2023-12-26 14:56:24 +0100

    Ulike variabler

---
+++ oving_java/variabel_testing.java
@@ -0,0 +1,12 @@
+public class variabel_testing {
+    public static void main (String [] args) {
+        String setning1 = "Det var en gang et menneske!";
+        int aar = 18;
+        String setning2 = "år gammel var han.";
+        double lonn = 250_000.54;
+
+
+        System.out.println(setning1 + " " + aar + " " + setning2);
+        System.out.println("Han tjente " + lonn + " kroner i året");
+    }
+}
