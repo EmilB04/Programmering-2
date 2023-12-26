296a80e740e904bf6cb7e9b2d9fde238ff6b7217
EmilB04 <emil.berglund@live.no>
2023-12-26 15:13:27 +0100

    Pluss og minus

---
+++ oving_java/plus_og_minus.java
@@ -0,0 +1,12 @@
+public class plus_og_minus {
+    public static void main (String[] args) {
+        int zebrasInZoo = 8;
+        int giraffesInZoo = 4;
+
+        int animalsInZoo = zebrasInZoo + giraffesInZoo; // Kunne også lagt på en parantes rundt disse
+        System.out.println(animalsInZoo);
+
+        int numZebrasAfterTrade = zebrasInZoo - 2;     // Kunne lagt på parantes her
+        System.out.println(numZebrasAfterTrade);
+    }
+}
