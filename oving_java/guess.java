baeec18712cf9fa25077b00cf4fc36e124691d2c
EmilB04 <emil.berglund@live.no>
2024-01-05 17:09:24 +0100

    Opplastning

--- oving_java/guess.java
+++ oving_java/guess.java
@@ -1,20 +1,22 @@
 import java.util.Scanner;
 
-public class while {
+public class guess {
     public static void main (String[] args){
         //make random number
         // from 1 to 100
         int number = (int)(Math.random() * 100 + 1);
         Scanner input = new Scanner(System.in);
+        
         System.out.println("Guess a magic number between 1 and 100");
-        int number_of_guesses = 0;
+        int number_of_guesses = 1;
+        int user_guess;
         while (true) {
-            guess = input.nextInt();
-            if (guess == number) {
+            user_guess = input.nextInt();
+            if (user_guess == number) {
                 System.out.println("You guessed correctly!");
                 break;
             }
-            else if (guess > number) {
+            else if (user_guess > number) {
                 System.out.println("Your guess is too high");
             }
             else {
