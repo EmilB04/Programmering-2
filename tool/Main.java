a8e46176a19fa10363457d6fbd44d72209a26a95
EmilB04 <emil.berglund@live.no>
2024-02-20 09:49:47 +0100

    Forelesnjng

--- tool/Main.java
+++ tool/Main.java
@@ -1,3 +1,17 @@
+import tools.UnitConverter;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        double resultInMeter1 = UnitConverter.cmToMeter(500);
+        System.out.println(resultInMeter1);
+
+        double resultInCm1 = UnitConverter.meterToCM(42);
+        System.out.println(resultInCm1);
+
+        double resultInKm1 = UnitConverter.meterToKm(2500);
+        System.out.println(resultInKm1);
+
+        double resultInMeter2 = UnitConverter.kmToMeter(3);
+        System.out.println(resultInMeter2);
+    }
 }
--- tool/tools/UnitConverter.java
+++ tool/tools/UnitConverter.java
@@ -1,10 +1,20 @@
 package tools;
 
 public class UnitConverter {
-    public double cmToMeter (double cm){
-        return cm / 100;
+    private static final int CM_METER_FACTOR = 100;
+    private static final int METER_KM_FACTOR = 1000;
+
+
+    public static double cmToMeter (double cm){
+        return cm / CM_METER_FACTOR;
     }
-    public double meterToCM (double meter){
-        return meter * 100;
+    public static double meterToCM (double meter){
+        return meter * CM_METER_FACTOR;
+    }
+    public static double meterToKm (double meter){
+        return meter / METER_KM_FACTOR;
+    }
+    public static double kmToMeter (double km){
+        return km * METER_KM_FACTOR;
     }
 }
