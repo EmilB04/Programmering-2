7524a13bdd208bcdaeacdab7136c60c56e37ff81
EmilB04 <emil.berglund@live.no>
2024-02-20 09:24:46 +0100

    Nytt prosjekt

---
+++ tool/tools/UnitConverter.java
@@ -0,0 +1,10 @@
+package tools;
+
+public class UnitConverter {
+    public double cmToMeter (double cm){
+        return cm / 100;
+    }
+    public double meterToCM (double meter){
+        return meter * 100;
+    }
+}
---
+++ yrkespersoner/final/final_variabel/src/no/hiof/emilbe/Final.java
@@ -0,0 +1,10 @@
+package no.hiof.emilbe;
+
+import no.hiof.emilbe.models.Person;
+
+public class Final {
+    public static void main(String[] args) {
+        Person person1 = new Person("21231231231");
+        System.out.println(person1.getBirthNumber());
+    }
+}
