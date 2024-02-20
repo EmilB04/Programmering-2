b4162596c1e63fd0200f86f9900cb2f22177cc35
EmilB04 <emil.berglund@live.no>
2024-02-20 08:33:03 +0100

    Opprettet nytt prosjekt

---
+++ yrkespersoner/final/final_variabel/src/no/hiof/emilbe/inheritance/Final.java
@@ -0,0 +1,7 @@
+package no.hiof.emilbe.inheritance;
+
+public class Final {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ yrkespersoner/final/final_variabel/src/no/hiof/emilbe/inheritance/models/Person.java
@@ -0,0 +1,11 @@
+package no.hiof.emilbe.inheritance.models;
+
+public class Person {
+    //private final String birthNumber = "12345678910";
+    private final String birthNumber;
+
+    public Person(String birthNumber) {
+        this.birthNumber = birthNumber;
+    }
+
+}
