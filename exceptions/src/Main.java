322022adf1517c88e23034d0d5ea90b108136577
EmilB04 <emil.berglund@live.no>
2024-03-07 13:43:46 +0100

    Forelesning - input

--- exceptions/src/Main.java
+++ exceptions/src/Main.java
@@ -1,10 +1,28 @@
+import java.io.BufferedReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main{
     public static void main(String[] args) {
-        simpleFileWriting("textFile.txt");
+        //simpleFileWriting("textFile.txt");
+        fileWritingFromInput("fileWritingFromInput.txt");
     }
+
+    public static void fileWritingFromInput(String fileName) {
+        try (FileWriter fileWriter = new FileWriter(fileName, true);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));){
+            
+            System.out.print("Din input her: ");
+            String inputText = bufferedReader.readLine();
+            fileWriter.append(inputText);
+        }
+        catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+
 
     public static void simpleFileWriting(String fileName) {
         try (FileWriter fileWriter = new FileWriter(fileName, false)) {
