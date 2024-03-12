cdc94a2e0c498867de2dfe6a9adce3ac42e08bc6
EmilB04 <emil.berglund@live.no>
2024-03-12 08:53:48 +0100

    Forelesning om filskriving

---
+++ filLesing/src/Main.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) {
+        // simpleFileWriting("textFile.txt");
+        //fileWritingFromInput("fileWritingFromInput.txt");
+        fileReading("fileWritingFromInput.txt");
+    }
+
+    public static void fileWritingFromInput(String fileName) {
+        try (FileWriter fileWriter = new FileWriter(fileName, true);
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));) {
+
+            while (true) {
+                System.out.print("Din input her: ");
+                String inputText = bufferedReader.readLine();
+
+                if (inputText.equals("quit") || inputText.equals("avslutt")) {
+                    break;
+                } else {
+                    fileWriter.append(inputText + "\n");
+                }
+                System.out.println("Skriv en av de oppgitte utrykkene for og avslutte: 'quit' eller 'avslutt'");
+            }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public static void fileReading(String fileName) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
+        }
+        catch (FileNotFoundException e) {
+            System.err.println(e.getMessage());
+        }
+        catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+
+
+    public static void simpleFileWriting(String fileName) {
+        try (FileWriter fileWriter = new FileWriter(fileName, false)) {
+            fileWriter.append("something..\n");
+            fileWriter.append("something else...\n");
+        }
+
+        catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+}
