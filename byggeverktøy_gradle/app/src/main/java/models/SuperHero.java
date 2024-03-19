8684675d9b429214aa133542ee02d9b24f126ca6
EmilB04 <emil.berglund@live.no>
2024-03-19 13:50:46 +0100

    Forelesning

---
+++ byggeverktøy_gradle/app/src/main/java/models/SuperHero.java
@@ -0,0 +1,31 @@
+package models;
+import java.util.ArrayList;
+
+
+public class SuperHero {
+    private String name;
+    private String realName;
+    private ArrayList<SuperHero> sideKicks = new ArrayList<>();
+
+    public SuperHero(String name, String realName){
+        this.name = name;
+        this.realName = realName;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setRealName(String realName){
+        this.realName = realName;
+    }
+    public String getRealName() {
+        return realName;
+    }
+    public ArrayList<SuperHero> getSideKicks() {
+        return sideKicks;
+    }
+}
